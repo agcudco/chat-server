e316445d97ec8d1c41794645918fd1b34afb2f25
agcudco <agcudco@espe.edu.ec>
2025-05-12 14:59:37 -0500

    version comentada

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ require('dotenv').config();
 
 // Importa Express para crear el servidor HTTP
 const express = require('express');
+
 // Importa el módulo HTTP de Node.js para envolver Express
 const http = require('http');
+
 // Importa CORS para permitir peticiones desde el cliente React
 const cors = require('cors');
+
 // Importa la clase Server de socket.io para habilitar WebSockets
 const { Server } = require('socket.io');
+
 // Importa DNS para resolver nombres de host a partir de IPs
 const dns = require('dns');
 
 // Crea la aplicación de Express
 const app = express();
+
 // Configura CORS usando el origen que definimos en .env
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
@@ -27,19 +32,23 @@ const io = new Server(server, {
 
 // Maneja las nuevas conexiones de clientes Socket.IO
 io.on('connection', (socket) => {
+
     // Extrae la dirección IP del cliente (elimina el prefijo IPv6 si existe)
     const clientIp = socket.handshake.address.replace('::ffff:', '');
 
     // Intenta resolver el nombre de host inverso para la IP del cliente
     dns.reverse(clientIp, (err, hostnames) => {
+
         // Si hay un hostname válido, lo usamos; si no, dejamos la IP
         const clientHost = (!err && hostnames.length) ? hostnames[0] : clientIp;
+
         // Envía al cliente su propia info de host e IP
         socket.emit('host_info', { host: clientHost, ip: clientIp });
     });
 
     // Escucha mensajes entrantes del cliente bajo el evento 'send_message'
     socket.on('send_message', (msg) => {
+        
         // Reenvía ese mensaje a todos los clientes conectados
         io.emit('receive_message', msg);
     });
